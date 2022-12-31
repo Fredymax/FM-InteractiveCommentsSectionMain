cdf726d5893d774bc2e7c6a60913f22b64d7ba42
Fredymax <CH4V3Zfredy@gmail.com>
2022-12-30 20:08:40 -0500

    Update render avatar

--- src/components/CommentInformation/CommentInformation.vue
+++ src/components/CommentInformation/CommentInformation.vue
@@ -3,7 +3,7 @@
     <div class="user">
       <div class="user__information">
         <figure class="user__avatar">
-          <img :src="avatar" class="user__photo" :alt="user?.username" />
+          <img :src="getAvatar(user?.image?.png)" class="user__photo" :alt="user?.username" />
         </figure>
         <strong class="user__name">{{ user?.username }}</strong>
         <span v-if="Auth.id === user?.id" class="user__you">You</span>
@@ -20,7 +20,7 @@
 </template>
 
 <script setup>
-import { toRefs, computed, watchEffect, ref } from "vue";
+import { toRefs, computed } from "vue";
 import CommentUpdate from "@/components/CommentUpdate/CommentUpdate.vue";
 import useUsers from "@/composables/useUsers";
 import { getAvatar } from "@/utils";
@@ -39,14 +39,7 @@ const { id, content, createdAt, userId, isEdit } = toRefs(props);
 const { getUserById, Auth } = useUsers();
 const user = computed(() => getUserById(userId.value));
 
-const avatar = ref(null);
-watchEffect(() => {
-  if (user.value) {
-    avatar.value = getAvatar(user.value.image.webp);
-  }
-});
-
-const renderAvatar = (nameAvatar) => getAvatar(nameAvatar);
+// const renderAvatar = (nameAvatar) => getAvatar(nameAvatar);
 
 function transformContent(content = "") {
   if (content.length == 0) return content;

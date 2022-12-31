1726d22b6c70310e4f6ff6abb95cbb79d5aa67c5
Fredymax <CH4V3Zfredy@gmail.com>
2022-12-30 19:53:44 -0500

    Update function getAvatar of utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,7 @@ export const getNumberRandom = () => {
 export const getLikes = () => getNumberRandom();
 
 export const getAvatar = (nameAvatar) => {
-  const pathname = `../assets/images/avatars/${nameAvatar}`;
+  const pathname = `/src/assets/images/avatars/${nameAvatar}`;
   const urlImage = new URL(pathname, import.meta.url);
   return urlImage.href;
 };
